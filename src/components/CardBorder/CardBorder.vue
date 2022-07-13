7beb094b1bb515b3a06e3ff8dfb83d2c10243186
Robin Scheidegger <robin.scheidegger@swisscom.com>
2022-07-13 10:03:24 +0200

    feat(CardBorder): try to resolve ts import bug

--- src/components/CardBorder/CardBorder.vue
+++ src/components/CardBorder/CardBorder.vue
@@ -2,4 +2,5 @@
   <div class="card-border title-1">test</div>
 </template>
 
+<script lang="ts" src="@/components/CardBorder/CardBorder.ts" />
 <style scoped lang="scss" src="@/components/CardBorder/CardBorder.scss" />
