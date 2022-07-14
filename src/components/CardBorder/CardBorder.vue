c9bafae378b99282b57a5af0ddc2dc2c8e4d28f9
Robin Scheidegger <robin.scheidegger@swisscom.com>
2022-07-14 12:02:47 +0200

    feat(CardBroder): create a single file component

--- src/components/CardBorder/CardBorder.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="card-border title-1">test</div>
-</template>
-
-<script lang="ts" src="@/components/CardBorder/CardBorder.ts" />
-<style scoped lang="scss" src="@/components/CardBorder/CardBorder.scss" />
