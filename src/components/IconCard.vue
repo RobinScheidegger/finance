2b717307bedbd89be7fb934f824cdff8288f0326
Robin Scheidegger <robin.scheidegger@swisscom.com>
2022-07-14 20:53:31 +0200

    feat(IconCard): create file

---
+++ src/components/IconCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="icon-card">
+    <img class="icon-card__icon" src="@/assets/icons/icon-settings.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IconCard",
+  props: {
+    icon: {
+      type: String,
+      default: "icon-settings",
+    },
+    size: {
+      type: Number,
+      default: 24,
+    },
+  },
+};
+</script>
