361fbc67bccaebe70f20d49f225ac7fd3fa865e3
Robin Scheidegger <robin.scheidegger@swisscom.com>
2022-07-18 21:22:32 +0200

    feat(MonthlySavingCard): done the layout and styling

--- src/components/DevPreview.vue
+++ src/components/DevPreview.vue
@@ -1,56 +1,13 @@
 <template>
-  <PaymentOverview
-    :paymentArray="[
-      {
-        type: 'payment',
-        date: new Date('2021-11-01'),
-        description: 'Essen in London',
-        category: ['Essen'],
-        amount: 30.5,
-      },
-      {
-        type: 'payment',
-        date: new Date('2021-11-01'),
-        description: 'Neues Crash',
-        category: ['Schlagzeug'],
-        amount: 140.8,
-      },
-      {
-        type: 'salary',
-        date: new Date('2021-11-01'),
-        description: 'Lohn',
-        category: ['Lohn'],
-        amount: 764,
-      },
-      {
-        type: 'save-money',
-        date: new Date('2021-11-01'),
-        description: 'Sparen',
-        category: ['Sparen'],
-        amount: 400,
-      },
-    ]"
-    :filterArray="[]"
-    @clickedCard="(i) => test1(i)"
-    @clickedCreateNew="test2()"
-  >
-  </PaymentOverview>
+  <MonthlySavingCard />
 </template>
 
 <script>
-import PaymentOverview from "@/components/PaymentOverview.vue";
+import MonthlySavingCard from "@/components/MonthlySavingCard.vue";
 export default {
   name: "DevPreview",
   components: {
-    PaymentOverview,
-  },
-  methods: {
-    test1(index) {
-      console.log("card ", index);
-    },
-    test2() {
-      console.log("create new");
-    },
+    MonthlySavingCard,
   },
 };
 </script>
---
+++ src/components/MonthlySavingCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="monthly-saving-card">
+    <div class="monthly-saving-card__content">
+      <div class="content__text subtitle-3">Schlagzeug Reparatur</div>
+      <div class="content__amount">
+        <div class="amount__current subtitle-2">CHF 150.00</div>
+        <div class="amount__divider subtitle-2">/</div>
+        <div class="amount__monthly title-3">CHF 100.00</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="typescript">
+export default {
+  name: "MonthlySavingCard",
+};
+</script>
+
+<style lang="scss">
+.monthly-saving-card {
+  background-color: #ffffff;
+  border: 1px solid #e1e1e1;
+  border-radius: 12px;
+
+  &__content {
+    margin: 15px 15px 10px 15px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+
+    .content__text {
+      color: #333333;
+    }
+
+    .content__amount {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+</style>
