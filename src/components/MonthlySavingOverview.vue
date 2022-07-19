91dca89fdad0c0db672ab76f4cb7d6c45e5ca923
Robin Scheidegger <robin.scheidegger@swisscom.com>
2022-07-19 20:54:33 +0200

    feat(MonthlySavingOverview): created file

---
+++ src/components/MonthlySavingOverview.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="monthly-saving-overview">
+    <div class="monthly-saving-overview__card">
+      <MonthlySavingCard />
+    </div>
+  </div>
+</template>
+
+<script lang="typescript">
+import MonthlySavingCard from "@/components/MonthlySavingCard.vue";
+
+export default {
+  name: "MonthlySavingCard",
+  components: {
+    MonthlySavingCard,
+  },
+  props: {
+    saving: {
+        type: Object,
+        default: () => ({})
+    }
+  }
+};
+</script>
+
+<style lang="scss"></style>
